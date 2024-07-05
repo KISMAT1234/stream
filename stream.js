44bd1539b10a186be11945d727d0541a1f790445
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-05 10:07:41 +0545

    writing stream from chunk

--- stream.js
+++ stream.js
@@ -1,12 +1,21 @@
-const {Readable} = require('stream')
+const {Readable,Writable} = require('stream')
 
 const readableStream = new Readable({
+    highWaterMark: 2,
     read() {}
+})
+
+const WritableStream = new Writable({
+write(s){
+    console.log('writing',s.toString());
+}
 })
 
 readableStream.on('data', (chunk)=>{
     console.log('Data coming',chunk);
     console.log('Data coming',chunk.toString());
+    WritableStream.write(chunk)
 })
 
 readableStream.push('Hello from kismat')
+
