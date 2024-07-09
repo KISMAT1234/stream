1af3d761f42613be0f985f057143e35ca5b96725
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-09 19:04:50 +0545

    sending objects in buffer

--- stream.js
+++ stream.js
@@ -1,21 +1,41 @@
 const {Readable,Writable} = require('stream')
 
+// const readableStream = new Readable({
+//     highWaterMark: 2,
+//     read() {}
+// })
+
+// const WritableStream = new Writable({
+// write(s){
+//     console.log('writing',s.toString());
+// }
+// })
+
+// readableStream.on('data', (chunk)=>{
+//     console.log('Data coming in chunk',chunk);
+//     console.log('Data coming',chunk.toString());
+//     WritableStream.write(chunk)
+// })
+
+// readableStream.push('Hello from kismat')
+
+
+
+
+
+
+// Accessing  objects in buffer using objectMode 
 const readableStream = new Readable({
+    objectMode:true,
     highWaterMark: 2,
     read() {}
 })
-
-const WritableStream = new Writable({
-write(s){
-    console.log('writing',s.toString());
-}
-})
-
 readableStream.on('data', (chunk)=>{
     console.log('Data coming in chunk',chunk);
-    console.log('Data coming',chunk.toString());
-    WritableStream.write(chunk)
+    // console.log('Data coming',chunk.toString());
+    // WritableStream.write(chunk)
 })
-
-readableStream.push('Hello from kismat')
+console.log(readableStream.push({
+    name:'roshan',
+}))
 
