f73d0df522a7bcb1365e70c12b04ba30d3c07263
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-16 22:14:12 +0545

    cpu intensive task created

---
+++ cluster/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+// app.get('/non-blocking',(req,res)=>{
+//     res.status(200).send("This page is non blocking");
+
+// })
+
+app.get('/heavy',(req,res)=>{
+    let counter = 0;
+    for(let i=0;i<1000000000;i++){
+        counter++;
+    }
+    res.send(`the result of cpu intensive work is ${counter}`)
+
+  
+})
+
+app.listen(8000,()=>{
+    console.log(`App listening on port 8000`)
+})
