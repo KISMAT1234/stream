7d9e8e7751564a1b49c42f89c3b360ebb6c33b11
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-17 21:09:15 +0545

    cluster initialized and created 8 server through cpu

--- cluster/primary.js
+++ cluster/primary.js
@@ -1,2 +1,25 @@
-const cluster = require('cluster')
-const os = require('os')
+const cluster = require('cluster');
+const os = require('os');
+const path = require('path');
+
+const newPath = path.dirname(__filename);
+const cpuCount = os.cpus().length;
+
+console.log(`CPU count: ${cpuCount}`);
+console.log(`Primary pid = ${process.pid}`);
+
+if (cluster.isPrimary) {
+    cluster.setupPrimary({
+        exec: path.join(newPath, 'index.js'),
+    });
+
+    for (let i = 0; i < cpuCount; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} has been killed`);
+        console.log('Starting another worker');
+        cluster.fork();
+    });
+}
