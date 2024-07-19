9b03c7e0f9d5b84a2ca0585b3476b53278d0d04a
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-19 18:39:14 +0545

    express added

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const app= express();
 const mongoose = require('mongoose');
+const app= express();
+app.use(express.json())
+app.use("/users", userRoutes);
 
 const Database = async() => {
 try{
     await mongoose.connect('mongodb+srv://kismat:kismat@cluster0.mnlkha6.mongodb.net/donation');
-    console.log('MongoDb connected');
+    // console.log('MongoDb connected');
 } catch(err){
-    console.log('connection failed');
-    process.exit(1)
+    // console.log('connection failed');
 }
 }
 Database()
@@ -20,9 +21,8 @@ app.post('/api/',(req,res)=>{
 const PORT = 8080
 app.listen(PORT,()=> console.log(`Listening on port ${PORT}`))
 
-module.exports={
-    app
-}
+module.exports=app
+
 
 
 
