dc7d9a4f3f441c04f0901352865f02c7d84bdb84
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-19 18:39:23 +0545

    file change

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,33 @@
-const {add} = require('../index.js')
+// const {add} = require('../index.js')
+const request = require('supertest')
+const app = require('../index.js')
 
-test('toBe',()=>{
-    expect(add(1,2)).toBe(3)
+
+
+// test('toBe',()=>{
+//     expect(add(1,2)).toBe(3)
+// })
+test('should signup for a user',async () => {
+    await request(app).post('/user').send({
+        username:'kismat',
+        email:'kismat@gmail.com',
+        password:'kismat'
+    }).expect(200)
 })
+
+test("should get all users", async () => {
+    const res = await request(app).get("/users");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("length");
+  });
+
+// describe("User API",()=>{
+//     describe("given a username and password",()=>{
+//         test("should respond with a 200 status code",()=>{
+
+//         })
+//     })
+//     describe("when the username and password are missing",()=>{
+
+//     })
+// })
