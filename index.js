2432957a00e21d3e65033030528dc0e2d1584b57
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-19 12:58:08 +0545

    mongodb connected and server statrted

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-const add = (a,b) => {
-    return a + b
+const express = require('express');
+const app= express();
+const mongoose = require('mongoose');
+
+const Database = async() => {
+try{
+    await mongoose.connect('mongodb+srv://kismat:kismat@cluster0.mnlkha6.mongodb.net/donation');
+    console.log('MongoDb connected');
+} catch(err){
+    console.log('connection failed');
+    process.exit(1)
+}
+}
+Database()
+app.post('/api/',(req,res)=>{
+    res.sendStatus(200)
+})
+
+
+const PORT = 8080
+app.listen(PORT,()=> console.log(`Listening on port ${PORT}`))
+
+module.exports={
+    app
 }
 
-module.exports = {
-    add
-}
+
+
+// const add = (a,b) => {
+//     return a + b
+// }
+
+// module.exports = {
+//     add
+// } 
+
+
+
+
