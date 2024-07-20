e4d82c4eebff5966ee1828e654a5625bf702a406
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-20 22:03:37 +0545

    password created using crypto

--- child-process/index.js
+++ child-process/index.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const crypto = require('crypto')
+
 setTimeout(()=>console.log('hello from timer'),0)
 setImmediate(()=>console.log('hello from setImmediate'))
 
@@ -8,7 +10,9 @@ fs.readFile('non_existent_file.txt',(err,data)=>{
     setTimeout(()=>console.log('hello from timer 3'),4000)
     setImmediate(()=>console.log('hello from setImmediate 2'))
 
-
+    crypto.pbkdf2('passsword','salt1',100000,1024,'sha512',()=>{
+        console.log('Password 1 done')
+    })
 })
 console.log('From top level code')
 
