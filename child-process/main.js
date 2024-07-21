3e2b94d890cb51e11b72b0c85deead096e9f7ce6
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-21 19:40:49 +0545

    execfile implement to make new thread instance

--- child-process/main.js
+++ child-process/main.js
@@ -1,4 +1,75 @@
-const  {exec, execFile, spawn,fork} = require('child_process')
-const path = require('path')
-const {fileUrlToPath} = require('url')
-const {dirname} = require('path')
+const { exec, execFile, spawn, fork } = require('child_process');
+const path = require('path');
+
+// __dirname is available by default in CommonJS
+// const __filename = path.resolve();  // This sets __filename correctly
+
+const __dir = path.dirname(__filename);
+ const start = Date.now();
+ console.log(start,'main file date')
+
+
+let counter = 0;
+for(i=0;i<1000000000; i++){
+    counter++;
+}
+console.log(`${Date.now()-start}ms Processing ${counter} in main file`)
+
+
+console.log('before')
+setTimeout(() =>console.log(` timer`),2000)
+//execFile
+const fileProcessorPath = path.resolve(__dir, 'execFileProcessor.js');
+execFile('node',[fileProcessorPath], (error, stdout, stderr) => {
+    console.log('inside')
+        if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout:\n${stdout}`)
+});
+console.log('after') 
+setTimeout(() =>console.log('timer 2'),0)
+
+// let abs = 0;
+// for(i=0;i<1000000000; i++){
+//     abs++;
+// }
+// console.log(`${Date.now()-start}ms Processing ${abs} in main but at last`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //exec
+            // exec('ls -lh', (error, stdout, stderr) => {
+            //     if (error) {
+            //         console.log(`error: ${error.message}`);
+            //         return;
+            //     }
+            //     if (stderr) {
+            //         console.log(`stderr: ${stderr}`);
+            //         return;
+            //     }
+            //     console.log(`stdout:\n${stdout}`);
+            // });
