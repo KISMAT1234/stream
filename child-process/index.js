2b6c1cbecce1269324dc234e0ab48ee9633e074f
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-21 12:59:29 +0545

    checking crypto asynchronous behaviour

--- child-process/index.js
+++ child-process/index.js
@@ -1,20 +1,48 @@
 const fs = require('fs');
 const crypto = require('crypto')
+const start  = Date.now()
 
 setTimeout(()=>console.log('hello from timer'),0)
 setImmediate(()=>console.log('hello from setImmediate'))
 
+
+
+
 fs.readFile('non_existent_file.txt',(err,data)=>{
     console.log('File read completed')
     setTimeout(()=>console.log('hello from timer 2'),0)
-    setTimeout(()=>console.log('hello from timer 3'),4000)
+    setTimeout(()=>console.log(`${Date.now()-start}ms timer 3`),4000)
     setImmediate(()=>console.log('hello from setImmediate 2'))
 
     crypto.pbkdf2('passsword','salt1',100000,1024,'sha512',()=>{
-        console.log('Password 1 done')
+        console.log(`${Date.now()-start}ms`,'Password 1 done')
     })
+    crypto.pbkdf2('passsword','salt1',100000,1024,'sha512',()=>{
+        console.log(`${Date.now()-start}ms`,'Password 2 done')
+    })
+    crypto.pbkdf2('passsword','salt1',100000,1024,'sha512',()=>{
+        console.log(`${Date.now()-start}ms`,'Password 3 done')
+    })
+    crypto.pbkdf2('passsword','salt1',100000,1024,'sha512',()=>{
+        console.log(`${Date.now()-start}ms`,'Password 4 done')
+    })
+    crypto.pbkdf2('passsword','salt1',100000,1024,'sha512',()=>{
+        console.log(`${Date.now()-start}ms`,'Password 5 done')
+    })
+
+    console.log('after crypto');
+    setTimeout(()=>console.log('after crypto from timer '),0)
+
+    
+
+
 })
+
 console.log('From top level code')
 
-
+let counter = 0;
+for(let i=0;i<1000000000;i++){
+    counter++;
+}
+console.log(`${Date.now()-start}ms and ${counter} is counted number`)
                       
