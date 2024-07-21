f7fc693744491bba6cba3902b4b25e8c9a631b2a
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-21 19:41:58 +0545

    Added time to check data

--- child-process/execFileProcessor.js
+++ child-process/execFileProcessor.js
@@ -1,1 +1,10 @@
+// const start = require('./main')
+let start = Date.now()
+console.log(start,'start value')
 console.log('processing data...')
+let counter = 0;
+for(i=0;i<100000000; i++){
+    counter++;
+}
+console.log(`${Date.now()-start}ms Processing ${counter} entries finished in execfile`)
+console.log('execfile after cpu intensive task')
