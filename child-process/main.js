8ef08a82790c3f4a189fa89c90279cc5e7f5d406
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-22 19:50:27 +0545

    spwan added

--- child-process/main.js
+++ child-process/main.js
@@ -6,34 +6,47 @@ const path = require('path');
 
 const __dir = path.dirname(__filename);
  const start = Date.now();
- console.log(start,'main file date')
+//  console.log(start,'main file date')
 
 
-let counter = 0;
-for(i=0;i<1000000000; i++){
-    counter++;
-}
-console.log(`${Date.now()-start}ms Processing ${counter} in main file`)
+ //exec
+// exec('ls -lh', (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout:\n${stdout}`);
+// });
+
+// let counter = 0;
+// for(i=0;i<1000000000; i++){
+//     counter++;
+// }
+// console.log(`${Date.now()-start}ms Processing ${counter} in main file`)
 
 
-console.log('before')
-setTimeout(() =>console.log(` timer`),2000)
-//execFile
-const fileProcessorPath = path.resolve(__dir, 'execFileProcessor.js');
-execFile('node',[fileProcessorPath], (error, stdout, stderr) => {
-    console.log('inside')
-        if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-                console.log(`stdout:\n${stdout}`)
-});
-console.log('after') 
-setTimeout(() =>console.log('timer 2'),0)
+// console.log('before')
+// setTimeout(() =>console.log(` timer`),2000)
+// //execFile
+// const fileProcessorPath = path.resolve(__dir, 'execFileProcessor.js');
+// execFile('node',[fileProcessorPath], (error, stdout, stderr) => {
+//     console.log('inside')
+//         if (error) {
+//                 console.log(`error: ${error.message}`);
+//                 return;
+//             }
+//             if (stderr) {
+//                     console.log(`stderr: ${stderr}`);
+//                     return;
+//                 }
+//                 console.log(`stdout:\n${stdout}`)
+// });
+// console.log('after') 
+// setTimeout(() =>console.log('timer 2'),0)
 
 // let abs = 0;
 // for(i=0;i<1000000000; i++){
@@ -43,6 +56,20 @@ setTimeout(() =>console.log('timer 2'),0)
 
 
 
+//spwan
+const spwanedChild = spawn('find',['.']);
+spwanedChild.stdout.on('data',(data)=>{
+ console.log(`stdout:\n${data}`);
+})
+spwanedChild.stderr.on('data',(data)=>{
+    console.log(`stderr:\n${data}`);
+})
+spwanedChild.on('error',(error)=>{
+    console.log(`stdout:\n${data.message}`);
+   })
+spwanedChild.on('close',(code)=>{
+    console.log(`child process exited with  ${code}`);
+})
 
 
 
@@ -61,15 +88,6 @@ setTimeout(() =>console.log('timer 2'),0)
 
 
 
-            //exec
-            // exec('ls -lh', (error, stdout, stderr) => {
-            //     if (error) {
-            //         console.log(`error: ${error.message}`);
-            //         return;
-            //     }
-            //     if (stderr) {
-            //         console.log(`stderr: ${stderr}`);
-            //         return;
-            //     }
-            //     console.log(`stdout:\n${stdout}`);
-            // });
+
+
+
