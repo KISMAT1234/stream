7b280605b3486011a4b6ba403c05d15c6e559d19
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-27 22:16:09 +0545

    checking thread outside the worker

--- multithread/index.js
+++ multithread/index.js
@@ -10,17 +10,21 @@ app.get('/non-blocking',(req,res)=>{
 app.get('/blocking',(req,res)=>{
     const worker = new Worker("./worker.js")
     worker.on("message",(data)=>{
-        console.log(data,'data from worker')
+        // console.log(data,'data from worker')
         res.status(200).send(`This page is blocking ${data}`);
     })
 
     worker.on("error",(error)=>{
         res.status(200).send("Error");
     })
-
-  
 })
 
 app.listen(8000,()=>{
     console.log(`App listening on port 8000`)
 })
+
+// let counter = 0;
+// for(let i=0;i<10000000000;i++){
+//     counter++;
+// }
+// res.status(200).send("This page is blocking the main thread");
