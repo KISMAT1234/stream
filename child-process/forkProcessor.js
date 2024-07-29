64c9c05986c6948a68d4048e59c3b6f319941f98
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-29 21:42:34 +0545

    looped the task to make main thread busy

--- child-process/forkProcessor.js
+++ child-process/forkProcessor.js
@@ -1,9 +1,15 @@
-process.on('message',(msg)=>{
-    console.log('Messages from parent process:',msg)
-})
+// process.on('message',(msg)=>{
+//     console.log('Messages from parent process:',msg)
+// })
+
+// let counter = 0;
+// setInterval(()=>{
+//     process.send({counter: counter++})
+// },1000)
+// console.log('counter',counter)
 
 let counter = 0;
-setInterval(()=>{
-    process.send({counter: counter++})
-},1000)
-console.log('counter',counter)
+for(i=0;i<1000000000; i++){
+    counter++;
+}
+process.send(counter)
