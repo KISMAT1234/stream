c2d2a82eb748e4706b789deb7e19428610c53c1b
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-26 21:21:27 +0545

    checking aync code

--- cluster/index.js
+++ cluster/index.js
@@ -6,25 +6,54 @@ const app = express();
 
 // })
 
-app.get('/heavy',(req,res)=>{
-    let counter = 0;
-    for(let i=0;i<1000000000;i++){
-        counter++;
-    }
-    res.send(`the result of cpu intensive work is ${counter}`)
+// app.get('/heavy2',(req,res)=>{
+   
+// console.log('hello from top')
+// process.env.UV_THREADPOOL_SIZE
+// console.log(process.env.UV_THREADPOOL_SIZE,'thread pool size')
+    
+    // let counter = 0;
+    // for(let i=0;i<10000000000;i++){
+    //     counter++;
+    // }
+    // setTimeout(()=>{
+    //     console.log('timer on ')
+    // },5000)
+
+    // console.log(counter);
+    // console.log('hello from below')
+
+ 
+    // res.send(`the result of cpu intensive work is heavy2 ${counter}`)  
+// })
+
+
+// app.get('/heavy3',(req,res)=>{
+//     let counter = 0;
+//     for(let i=0;i<1000000000;i++){
+//         counter++;
+//     }
+//     res.send(`the result of cpu intensive work is heavy3 ${counter}`)
 
   
-})
+// })
+// app.get('/heavy1',(req,res)=>{
+//     let counter = 0;
+//     for(let i=0;i<10000000000;i++){
+//         counter++;
+//     }
+//     res.send(`the result of cpu intensive work is heavy1 ${counter}`)
+// })
 
-app.listen(8000,()=>{
-    console.log(`App listening on port 8000`)
-})
+// app.listen(8000,()=>{
+//     console.log(`App listening on port 8000`)
+// })
 
 
-//loadtesting 
+//loadtesting ---------
 // npx loadtest -n 1200 -c 400 -k http://localhost:8000
 
-// pm2 memory usage test
+// pm2 memory usage test ------
 // npx pm2 start primary.js
 // npx pm2 monit
 // npx pm2 status
