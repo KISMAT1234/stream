5d6609bc27fce4eb6f83c4a07439b800d3eb92ba
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-26 21:19:22 +0545

    prisma client initialized

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
+// const express = require('express');
+// const mongoose = require('mongoose');
 
-const app= express();
-app.use(express.json());
-// app.use("/users", userRoutes);
+// const app= express();
+// app.use(express.json());
+// // app.use("/users", userRoutes);
 
-const Database = async() => {
-try{
-    await mongoose.connect('mongodb+srv://kismat:kismat@cluster0.mnlkha6.mongodb.net/donation');
-    // console.log('MongoDb connected');
-} catch(err){
-    // console.log('connection failed');
-    console.error(err);
-}
-};
-Database();
-app.post('/api/',(req,res)=>{
-    res.sendStatus(200);
-});
+// // const Database = async() => {
+// // try{
+// //     await mongoose.connect('mongodb+srv://kismat:kismat@cluster0.mnlkha6.mongodb.net/donation');
+// //     // console.log('MongoDb connected');
+// // } catch(err){
+// //     // console.log('connection failed');
+// //     console.error(err);
+// // }
+// // };
+// // Database();
+// app.post('/api/',(req,res)=>{
+//     res.sendStatus(200);
+// });
 
 
-const PORT = 8080;
-app.listen(PORT,()=> console.log(`Listening on port ${PORT}`));
 
-module.exports=app;
+// const PORT = 8080;
+// app.listen(PORT,()=> console.log(`Listening on port ${PORT}`));
+
+// module.exports=app;
 
 
 
@@ -36,6 +37,9 @@ module.exports=app;
 //     add
 // } 
 
+const { PrismaClient } =  require("@prisma/client");
+const prisma = new PrismaClient()
 
+module.exports = prisma
 
 
