5478230836a712c36aad198ef00fcc5864c35c2e
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-30 09:43:33 +0545

    file change

--- fileWriter.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-// const writeableStream = fs.createWriteStream('log.txt')
-// process.stdin.pipe(writeableStream) 
-
-const readableStream = fs.createReadStream('log.txt')
-
-readableStream.pipe(process.stdout)
