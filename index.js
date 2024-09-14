96079a75c954903d11cef60ff705d8591ee2930c
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-14 09:15:40 -0700

    changes minor change

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
 
-// const app= express();
-// app.use(express.json());
+const app= express();
+app.use(express.json());
 // // app.use("/users", userRoutes);
 
 // // const Database = async() => {
